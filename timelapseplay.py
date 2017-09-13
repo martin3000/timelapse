4349dcca04888ed5ead0ebf1e172d1d97eaccf0c
martin3000 <github@mx.schlatter.eu>
2017-09-13 09:56:15 +0200

    Create timelapseplay.py

---
+++ timelapseplay.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# play captured images as video
+# JMS 2017
+#
+
+import cv2
+import time
+
+print("Press 'q' to stop.")
+
+i=0
+cap = cv2.VideoCapture("snapshot-%08d.jpg")
+while True:
+    ret, image = cap.read()
+    if not ret: break
+
+    cv2.imshow(" ", image)
+    i += 1
+    k=cv2.waitKey(100)
+    if k & 0xFF == ord('q'):
+        print("stop")
+        break
+
+cap.release()           # clear buffer
+print("finished. processed {} images.".format(i))
