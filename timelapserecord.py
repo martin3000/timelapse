ee4fde38604a07c18988b91ae08246641d571c60
martin3000 <github@mx.schlatter.eu>
2017-09-13 14:47:09 +0200

    Update timelapserecord.py

--- timelapserecord.py
+++ timelapserecord.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# capture an image every x seconds 
+# capture a webcam image every 60 seconds 
 # JMS 2017
 #
 WAIT=60 # sleep time in seconds
@@ -8,17 +8,15 @@ import cv2
 import time
 
 print("Press 'q' to stop.")
-i=730
+i=0
 
 while True:
     cap = cv2.VideoCapture(0)
     cap.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH ,1024)
     cap.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 576)
     ret, image = cap.read()
-    print(ret)
     cap.release()           # clear buffer
     if not ret: 
-        print("ret nonzero")
         break
 
     output = "snapshot-{:08}.jpg".format(i) # 8 digits, leading zeros
@@ -28,7 +26,6 @@ while True:
     cv2.imshow(" ", image)
     k=cv2.waitKey(1000*WAIT)
     if k & 0xFF == ord('q'):         # does not work, always -1
-        print("stop")
         break
 
     print(time.strftime("%d %b %Y %H:%M:%S",time.localtime()))
